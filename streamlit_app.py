989edf1854c3e21bd5f14577603c44a7e714fbe0
Jeff Player - Snowflake <113489563+jdpd4bsnowflake@users.noreply.github.com>
2022-09-27 12:15:08 -0400

    Update streamlit_app.py
    Normalized json response and display in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize fruityvice json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display normalized json in a table grid
+streamlit.dataframe(fruityvice_normalized)
